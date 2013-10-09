mikemol/fireholv6

Move to Autotools and dblatex
 - Autotools for build and install
 - dblatex for PDF manual creation

Many thanks to Jerome BENOIT who did almost all the work to make these
changes.

doc/xsl/html.xsl
@@ -1,0 +1,7 @@
+<?xml version='1.0'?>
+<xsl:stylesheet  xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+<xsl:import href="http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl"/>
+<xsl:param name="html.stylesheet.type">text/css</xsl:param>
+<xsl:param name="html.stylesheet">sanewall-manual.css</xsl:param>
+<xsl:param name="generate.index">0</xsl:param>
+</xsl:stylesheet>

fawong/firehol

doc/xsl/html.xsl
@@ -1,0 +1,7 @@
+<?xml version='1.0'?>
+<xsl:stylesheet  xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+<xsl:import href="http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl"/>
+<xsl:param name="html.stylesheet.type">text/css</xsl:param>
+<xsl:param name="html.stylesheet">firehol-manual.css</xsl:param>
+<xsl:param name="generate.index">0</xsl:param>
+</xsl:stylesheet>
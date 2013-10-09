mikemol/fireholv6

Update adblock script
 - Make it run sanewall when it has finished getting IPs

admin/adblock.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash -e
 #
 # $Id: adblock.sh,v 1.1 2003/10/07 23:42:17 ktsaou Exp $
 # A script that will fetch the IPs of popular add servers.
+#  - Updated to run sanewall on success
 #
 # To use this, just put in your cron jobs, like this:
 #
@@ -19,8 +20,11 @@
 #
 #     blacklist full "${ADSERVERS_IPS}"
 #
-#
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/usr/sbin:/bin:/usr/bin
+export PATH
 
 printf "ADSERVERS_IPS=\""
 printf "%q " `wget -q -O - "http://pgl.yoyo.org/adservers/iplist.php?ipformat=plain&showintro=0" | egrep -e "^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\$"`
 printf "\"\n"
+
+sanewall start

fawong/firehol

 - Make it run firehol when it has finished getting IPs

admin/adblock.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash -e
 #
 # $Id: adblock.sh,v 1.1 2003/10/07 23:42:17 ktsaou Exp $
 # A script that will fetch the IPs of popular add servers.
+#  - Updated to run firehol on success
 #
 # To use this, just put in your cron jobs, like this:
 #
@@ -19,8 +20,11 @@
 #
 #     blacklist full "${ADSERVERS_IPS}"
 #
-#
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/usr/sbin:/bin:/usr/bin
+export PATH
 
 printf "ADSERVERS_IPS=\""
 printf "%q " `wget -q -O - "http://pgl.yoyo.org/adservers/iplist.php?ipformat=plain&showintro=0" | egrep -e "^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\$"`
 printf "\"\n"
+
+firehol start